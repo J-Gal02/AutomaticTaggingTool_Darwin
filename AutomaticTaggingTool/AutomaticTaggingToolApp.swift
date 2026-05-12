9a6b1f92147379e2d592c4591d76f3cd07ce89f4
Jared G <jared@droove.net>
2026-05-12 14:37:36 +1000

    Initial Commit

---
+++ AutomaticTaggingTool/AutomaticTaggingToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomaticTaggingToolApp.swift
+//  AutomaticTaggingTool
+//
+//  Created by jared on 12/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct AutomaticTaggingToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutomaticTaggingToolTests/AutomaticTaggingToolTests.swift
@@ -0,0 +1,19 @@
+//
+//  AutomaticTaggingToolTests.swift
+//  AutomaticTaggingToolTests
+//
+//  Created by jared on 12/5/2026.
+//
+
+import Testing
+@testable import AutomaticTaggingTool
+
+struct AutomaticTaggingToolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
